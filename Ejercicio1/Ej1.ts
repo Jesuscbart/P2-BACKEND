b52cddcb44108a09d10accc75d24eaf13691ad4d
Jesuscbart <jcb@jesuscuesta.es>
2023-10-11 21:10:59 +0200

    Comments added

--- Ejercicio1/Ej1.ts
+++ Ejercicio1/Ej1.ts
@@ -2,62 +2,48 @@
 //Ejercicio 1
 //Jesús Cuesta Bartolomé
 
-//Para este primer ejercicio se pide hacer una petición GET al siguiente endpoint: Quote Garden - Star on GitHub | QuoteGarden (pprathameshmore.github.io) 
-//Este servicio nos devolverá un listado de cuotas. Una vez obtengamos las cuotas deberemos imprimir por pantalla la información del siguiente modo.
-//Se debe cambiar la X por cada campo de la respuesta:
-//  "Genero: x Autor: x Quote: x"
-//Punto extra: Cada llamada imprime 10 cuotas, buscar la manera de obtener 30 cuotas distintas
-
-// Tipos
-export interface ResponseData { //Interfaz para el JSON que devuelve la API
-    statusCode:  number;
-    message:     string;
-    pagination:  Pagination;
-    totalQuotes: null;
-    data:        Data[];
+// Tipos de datos
+export interface ResponseData {
+    statusCode:  number;        //Cósigo de estado de la respuesta
+    message:     string;        //Mensaje de la respuesta
+    pagination:  Pagination;    //Paginación de la respuesta
+    totalQuotes: null;          //Cantidad total de citas
+    data:        Data[];        //Array de ojetos Data que contienen las citas
 }   
 
-export interface Data { //Interfaz para el array de datos que devuelve la API
-    _id: string;
-    quoteText: string;
-    quoteAuthor: string;
-    quoteGenre: string;
-    __v: number;
+export interface Data { 
+    _id: string;                //Identificador de la cita
+    quoteText: string;          //Texto de la cita
+    quoteAuthor: string;        //Autor de la cita
+    quoteGenre: string;         //Género de la cita
+    __v: number;                //Versión de la cita
 }
 
-export interface Pagination {   //Interfaz para la paginación que devuelve la API
-    currentPage: null;
-    nextPage:    null;
-    totalPages:  null;
+export interface Pagination { 
+    currentPage: null;          //Página actual
+    nextPage:    null;          //Página siguiente
+    totalPages:  null;          //Total de páginas
 }
-/*
-//Función asíncrona que hace la llamada a la API y muestra los datos por pantalla
-const printInfo = async () => {
-    try {
-      let quotes: Data[] = [];
-      for (let i = 1; i <= 3; i++) {
-        const response = await fetch(`https://quote-garden.onrender.com/api/v3/quotes?page=${i}&limit=10`);
-        const data: ResponseData = await response.json();
-        quotes = quotes.concat(data.data);
-      }
-      quotes.slice(0, 30).forEach((quote: Data) => {
-        console.log(`Genero: ${quote.quoteGenre} Autor: ${quote.quoteAuthor} Quote: ${quote.quoteText}`);
-        console.log("---------------------------------");
-      });
-    } catch (error) {
-      console.log("Error:", error);
-    }
-  };*/
 
-
+//Función asíncrona que imprime por consola los datos de las citas
   const printInfo = async () => {
     try {
-      const response = await fetch("https://quote-garden.onrender.com/api/v3/quotes?limit=30");
+
+      //Realiza una solicitud HTTP GET a la API para obeter las citas
+      const response = await fetch(
+        "https://quote-garden.onrender.com/api/v3/quotes?limit=30"  //Limito la cantidad de citas a 30 para poder imprimir 30 citas diferentes. 
+      );                                                            //Utilizo ? para indicar que voy a pasar un parámetro y pongo el parámetro limit con valor 30
+      
+      //Analiza la respuesta como JSON y asigna los datos a la variable data
       const data: ResponseData = await response.json();
+
+      //Itero sobre el array de citas y muestro por consola los datos de cada una
       data.data.forEach((quote: Data) => {
         console.log(`Genero: ${quote.quoteGenre} Autor: ${quote.quoteAuthor} Quote: ${quote.quoteText}`);
-        console.log("---------------------------------");
+        console.log("--------------------------------------------------------------------------------");
       });
+
+    // En caso de error, lo muestro por consola
     } catch (error) {
       console.log("Error:", error);
     }
--- README.md
+++ README.md
@@ -3,10 +3,44 @@ Para esta segunda práctica se pide que se realicen peticiones hacia una API RES
 
 ## Ejercicio 1
 Para este primer ejercicio se pide hacer una petición GET al siguiente endpoint: Quote Garden - Star on GitHub | QuoteGarden (pprathameshmore.github.io) 
+
 Este servicio nos devolverá un listado de cuotas. Una vez obtengamos las cuotas deberemos imprimir por pantalla la información del siguiente modo.
 Se debe cambiar la X por cada campo de la respuesta:
+
 "Genero: x Autor: x Quote: x"
+
 Punto extra: Cada llamada imprime 10 cuotas, buscar la manera de obtener 30 cuotas distintas
+
 Ejemplo practico: 
 example.ts
  
+## Ejercicio 2
+Para este segundo ejercicio se pide que el usuario sea capaz de escribir por consola el nombre de un Pokémon para después imprimir por pantalla su información. PokéAPI (pokeapi.co)
+
+Para esta parte del ejercicio se tendrá que usar la API de Deno Input Prompts - Deno by Example.
+
+Los datos que se deberán mostrar son:
+* Nombre
+* tipo/tipos
+* id
+
+Punto extra: Antes de pedir al usuario el nombre del Pokémon. Indicar con un 1 o un 2 si se quiere buscar un tipo o un Pokémon respectivamente. Para el tipo imprimir lo siguiente:
+* Tipo
+* Generación
+
+## Ejercicio 3
+Teniendo en cuenta el ejercicio anterior, se pide crear un programa CLI Command Line Interface para una biblioteca. El tipo de un Libro será:
+
+* id: Identificador único del libro. NO PUEDE HABER DOS IGUALES
+* title
+* author
+* pages
+* genre
+
+Tendremos las siguientes opciones:
+* 1 - Crear libro
+* 2 - Filtrar libro por género
+* 3 - Borrar libro
+* 4 - Salir
+
+Punto extra: Realizar test de cada función siguiendo la guía oficial de Deno
